ce3567a56fd179c1ca435c0ee5af9ac074edfa63
Duhan OVALI <60785283+duhanens@users.noreply.github.com>
2025-11-15 22:25:56 +0300

    Refactor disk_usage.sh for improved readability

--- disk_usage.sh
+++ disk_usage.sh
@@ -10,29 +10,29 @@ N=10
 while getopts "an:" opt; do
 	case "$opt" in
 		a)
-            		showALL=1
-            		;;
-        	n)
-            		N="$OPTARG"
-            		;;
-        	*)
-            		usage
-            		exit 1
-            		;;
-    	esac
+            showALL=1
+            ;;
+        n)
+            N="$OPTARG"
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
 done
 
 shift $((OPTIND - 1))
 
 if [ $# -lt 1 ]; then
 	usage
-    	exit 1
+    exit 1
 fi
 
 for dir in "$@"; do
-    	if [ "$showALL" -eq 1 ]; then
-        	du -ah "$dir" 2>/dev/null | sort -hr | head -n "$N"
-    	else
-        	du -h "$dir" 2>/dev/null | sort -hr | head -n "$N"
-    	fi
+    if [ "$showALL" -eq 1 ]; then
+        du -ah "$dir" 2>/dev/null | sort -hr | head -n "$N"
+    else
+        du -h "$dir" 2>/dev/null | sort -hr | head -n "$N"
+    fi
 done
